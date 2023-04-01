6ff097fe8d0110d9c6d40ebd851283f506f13900
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-04-01 13:41:43 +0200

    Es:SQL

--- src/main/java/view/Start.java
+++ src/main/java/view/Start.java
@@ -2,45 +2,40 @@ package main.java.view;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Start {
     public static void main(String[] args) throws NullPointerException {
         Connection conn = null;
         try {
             String url = "jdbc:mysql://localhost:3306/newdb";
-            String user = "developer";
+            String user = "root";
             String password = "Irisviel95!";
             conn = DriverManager.getConnection(url,user,password);
             Statement statement;
             ResultSet italians;
             ResultSet germans;
-            ArrayList<Student> italianStudents = new ArrayList<>();
-            ArrayList<Student> germanStudents = new ArrayList<>();
+            List<Student> italianStudents = new ArrayList<>();
+            List<Student> germanStudents = new ArrayList<>();
             try {
                 statement = conn.createStatement();
-                italians = statement.executeQuery("SELECT * FROM italian_students");
+                italians = statement.executeQuery("SELECT * FROM newdb.italian_students");
                 while (italians.next()){
-
-
-                    for (Student student:italianStudents) {
-                        student.surname = italians.getString("first_name");
-                        student.name = italians.getString(2);
-                        italianStudents.add(student);
-                    }
-
-                    System.out.println(italianStudents);
-
+                    String surname = italians.getString("first_name");
+                    String name = italians.getString(2);
+                    Student stud = new Student(name,surname);
+                    italianStudents.add(stud);
                 }
-                germans = statement.executeQuery("SELECT * FROM german_students");
+
+                germans = statement.executeQuery("SELECT * FROM newdb.german_students");
                 while (germans.next()){
-                    for (Student student:germanStudents) {
-                        student.surname = germans.getString("first_name");
-                        student.name = germans.getString(2);
-                        germanStudents.add(student);
-                    }
-                    System.out.println(germanStudents);
-
+                    String surname = germans.getString("first_name");
+                    String name = germans.getString(2);
+                    Student stud = new Student(name,surname);
+                    germanStudents.add(stud);
                 }
+                System.out.println(italianStudents);
+                System.out.println(germanStudents);
             }catch (SQLException e){
                 e.printStackTrace();
             }
@@ -54,6 +49,7 @@ public class Start {
                 System.out.println("SQLException: "+ e.getSQLState());
                 System.out.println("SQLException: "+ e.getErrorCode());
             }
+
         }
     }
 }
